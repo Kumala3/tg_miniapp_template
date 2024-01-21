01930b7009cef234ba9eae5862b5531302573c9e
Kostek <ksytnik73kwer@gmail.com>
2024-01-21 22:36:06 +0100

    Add betterlogging to "apu.requirements.txt". Update new dependencies in "package.json". Update Vite configuration file. Comment using "redis_cache".

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react(),],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+});
